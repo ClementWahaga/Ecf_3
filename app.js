d01981a7df386a2079e92970ea03ebc64130ec9c
Clement Wahaga <c.wahaga@cci-formation.nc>
2021-05-16 13:28:10 +0200

    update

--- app.js
+++ app.js
@@ -1,167 +1,153 @@
+// Get all users
 let list = document.getElementsByTagName("table")
 var table = document.getElementById("myTable");
-
-let xhr = new XMLHttpRequest()
 var url = "https://6057e432c3f49200173ad08d.mockapi.io/api/v1/employees/"
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        list.innerHtml = xhr.response
-        console.log(xhr.response);
-    }
 
 
+var xhr = new XMLHttpRequest()
+xhr.open('GET', url, true)
+xhr.onload = function() {
+    var data = JSON.parse(xhr.responseText);
+    if (xhr.readyState == 4 && xhr.status == "200") {
 
-}
-xhr.open("GET", url, true)
-xhr.responseType = 'json'
-xhr.send();
+        console.log(xhr);
 
-/*-------------------------------------------------creation du tableau------------------------------------------------------------------------ */
-
-function generate_table() {
-    var data = xhr.response
-    console.log(xhr);
-
-    for (var i = 0; i < data.length; i++) {
-        var row = `
-             <tr>
-                <td style="border: 2px solid black;">${data[i].email}</td> 
+        for (var i = 0; i < data.length; i++) {
+            var row = `
+             <tr id ='id' >
+                
                 <td style="border: 2px solid black;">${data[i].id}</td> 
-                <td style="border: 2px solid green;">${data[i].job_title}</td> 
                 <td style="border: 2px solid yellow;">${data[i].last_name}</td> 
-                <td style="border: 2px solid red;">${data[i].name}</td>
+                <td  style="border: 2px solid red;">${data[i].name}</td>
             
             
-                <td style="border: 2px solid black "><input type="button" value="Id" onclick="displayId()" id="display" style="cursor:pointer"></td>
-                <td style="border: 2px solid black "><input type="button" value="mod" onclick="modifyId()"style="cursor:pointer"></td>
-                <td style="border: 2px solid black;"><input type="button" value="del" onclick="deleteId()"style="cursor:pointer"></td> 
+                <td style="border: 2px solid black "><input type="button" value="view more"onclick="display()" style="cursor:pointer"></td>
+                <td style="border: 2px solid black "><input type="button" value="modify" onclick="modifyId()" style="cursor:pointer"></td>
+                <td style="border: 2px solid black;"><input type="button" value="delete" onclick="deleteId()" "style="cursor:pointer" ></td> 
             </tr>`
 
 
 
 
-        ;
-        table.innerHTML += row;
+            ;
+            table.innerHTML += row;
+
+
+        }
+
+
+
+
+
+
 
     }
-
-
+    console.table(data);
 
 }
-/*-------------------------------------------------affichez employees------------------------------------------------------------------------ */
+xhr.send();
 
-function displayId() {
-
-
-    data = xhr.response
-
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var t = Object.assign(this.response)
-            var u = Object.keys(t)
-            console.log(t);
-
-            console.log(u);
-
-            (alert('nom: ' + data['name'] + '\n' + 'prenom: ' + data['last_name'] + '\n' + 'emploi: ' + data['job_title']))
+/*----------------------------------------------------------------------------------displayUser----------------------------------------------------*/
 
 
 
+
+
+
+function display() {
+
+
+    var xhr = new XMLHttpRequest()
+
+    xhr.onload = function() {
+        var data = JSON.parse(xhr.responseText);
+        if (xhr.readyState == 4 && xhr.status == "200") {
+
+            for (var i = 0; i < data.length; i++) {
+                alert('id:' + data[i].id + '\n' + 'nom: ' + data[i].name + '\n' + 'prenom: ' + data[i].last_name + '\n' + 'emploi: ' + data[i].job_title + '\n' + 'email:' + data[i].email)
+            }
+
+            console.table(data);
+        } else {
+            console.error(data);
         }
-
-
-
     }
-    xhr.open("GET", "https://6057e432c3f49200173ad08d.mockapi.io/api/v1/employees/", true)
-    xhr.responseType = 'json'
-    xhr.send();
+}
+xhr.open('GET', url, true)
+xhr.send();
 
+
+
+
+/*----------------------------------------------------------------Create a user -------------------------------------------------------------------------------------------- */
+
+
+function create(onload) {
+    var newdata = {}
+    newdata.name = prompt("name")
+    newdata.last_name = prompt("lastname")
+    newdata.email = prompt("email")
+    newdata.job_title = prompt("job")
+    var json = JSON.stringify(newdata);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.onload = function() {
+        var employees = JSON.parse(xhr.responseText);
+        if (xhr.readyState == 4 && xhr.status == "201") {
+            console.table(employees);
+        } else {
+            console.error(employees);
+        }
+    }
+    xhr.send(json);
+    console.log(xhr.send(json));
 }
 
 
 
-/*-------------------------------------------------modifié employees------------------------------------------------------------------------ */
-function modifyId() {
-    var data = xhr.response
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            list.innerHtml = xhr.response
-            console.log(xhr.response);
+/*---------------------------------------------------------------------------------Update a user----------------------------------------------------------------------*/
+
+function modifyId(params) {
+    var data = {};
+    data.firstname = prompt("modifyName");
+    data.lastname = prompt("modifyLastname");
+    data.email = prompt("modifyEmail");
+    data.job_title = prompt("modifyJob")
+    var json = JSON.stringify(data);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("PUT", url + data, true);
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.onload = function() {
+        var data = JSON.parse(xhr.responseText);
+        if (xhr.readyState == 4 && xhr.status == "200") {
+            console.table(data);
+        } else {
+            console.error(data);
         }
-
-
-
     }
-    xhr.open("PUT", url_display, true)
-    xhr.responseType = 'json'
+    xhr.send(json);
+}
+
+
+
+/*----------------------------------------------------------------------------------------------------- Delete a user-------------------------------------------------------*/
+function deleteId(choise) {
+    var choise = confirm("voulais vous vraiment supprimer cet utilisateur")
+    var xhr = new XMLHttpRequest();
+    xhr.open("DELETE", url, true);
+    xhr.onload = function() {
+        var data = JSON.parse(xhr.responseText);
+        if (xhr.readyState == 4 && xhr.status == "200") {
+            data.target.push
+            console.table(data);
+        } else {
+            console.error(data);
+        }
+    }
     xhr.send();
 
-    var j = prompt('Modifier le nom', data.name);
-    if (j !== '') {
-        const newData = {
-
-            name: name,
-
-        };
-        data.name.replace(j, newData)
-
-    }
-
-    /*-------------------------------------------------supprimez employees------------------------------------------------------------------------ */
-    function deleteId() {
-        var data = xhr.response
-        xhr.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                list.innerHtml = xhr.response
-                console.log(xhr.response);
-            }
-
-
-
-        }
-        xhr.open("DELETE", url_display, true)
-        xhr.responseType = 'json'
-        xhr.send();
-        for (var i = 0; i < 100; i++) {
-            if (confirm(`Souhaitez vous supprimez l'employees :"data.name`)) {
-                data.name.remove(row)
-
-            }
-        }
-
-
-
-    }
-    /*-------------------------------------------------ajoutez employees------------------------------------------------------------------------ */
-    function create() {
-        var data = xhr.response
-
-        xhr.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 201) {
-
-                console.log(xhr.response);
-
-
-
-            }
-
-        }
-        xhr.open("POST", url, true)
-        xhr.responseType = 'json'
-        xhr.send(j, k, l, m);
-
-        var j = JSON.parse(prompt('Name:', data.name))
-        var k = JSON.parse(prompt('LastName:', data.last_name));
-        var l = JSON.parse(prompt('@mail:', data.email));
-        var m = JSON.parse(prompt('emploi:', data.job_title));
-
-
-
-
-
-
-
-
-    }
 }
